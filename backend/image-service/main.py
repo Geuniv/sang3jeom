67542eb7cc6940ef2ebb85ccffb723949a4d8d4d
joohyeongLee <joo9710@naver.com>
2025-08-13 16:39:46 +0900

    backend ci test

--- backend/image-service/main.py
+++ backend/image-service/main.py
@@ -14,7 +14,7 @@ import re
 
 load_dotenv()
 
-# HF_API_URL = os.getenv("HF_API_URL")
+# HF_API_URL = os.getenv("HF_API_URL") ci/cd test
 # HF_API_KEY = os.getenv("HUGGINGFACE_API_KEY")
 # ci 테스트
 UPLOAD_DIR = os.getenv("UPLOAD_DIR", "./uploads")
