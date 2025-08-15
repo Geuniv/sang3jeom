e3b6c468db7f0c278a80efd6e05d8b3ba0edbf0d
joohyeongLee <joo9710@naver.com>
2025-08-15 12:59:30 +0900

    test : user ci/cd

--- backend/user-service/src/main/java/com/example/demo/controller/AdminTokenController.java
+++ backend/user-service/src/main/java/com/example/demo/controller/AdminTokenController.java
@@ -22,7 +22,7 @@ public class AdminTokenController {
   private final TokenService tokenService;
 
   /**
-   * HTTP DELETE /admin/tokens/{jti} 8/14 ci/cd test(user)61231111
+   * HTTP DELETE /admin/tokens/{jti} 8/14 ci/cd test(user)61231111test
    * 해당 JTI를 화이트리스트에서 삭제하고, 블랙리스트에 기록합니다.
    */
   @DeleteMapping("/{jti}")
