c62c138734b51556ff5bf1cfff9300175544095a
joohyeongLee <joo9710@naver.com>
2025-08-17 23:15:33 +0900

    test : order-service ci/cd test

--- backend/order-service/src/main/java/com/sang3jeom/order_service/controller/OrderController.java
+++ backend/order-service/src/main/java/com/sang3jeom/order_service/controller/OrderController.java
@@ -23,7 +23,7 @@ public class OrderController {
     private final OrderService orderService;
     private final UserServiceClient userServiceClient;
 
-/*    @PostMapping citest6123123123123
+/*    @PostMapping citest6123123123123455555
     public ResponseEntity<CreateOrderResponse> createOrder(@RequestBody CreateOrderRequest request) {
         return ResponseEntity.ok(
                 orderService.createOrder(request));
