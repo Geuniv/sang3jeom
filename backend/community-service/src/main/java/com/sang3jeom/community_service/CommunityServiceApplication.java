1b3cdea28d1881d72378bd6ea9ce1c3daeb03611
joohyeongLee <joo9710@naver.com>
2025-08-18 11:19:58 +0900

    test : 헬스체크 추가 후 test(커뮤니티)

--- backend/community-service/src/main/java/com/sang3jeom/community_service/CommunityServiceApplication.java
+++ backend/community-service/src/main/java/com/sang3jeom/community_service/CommunityServiceApplication.java
@@ -15,7 +15,7 @@ public class CommunityServiceApplication {
 		SpringApplication.run(CommunityServiceApplication.class, args);
 	}
 
-	// === CORS 전역 허용 설정 추가 === citest6 123 8/18 test eee
+	// === CORS 전역 허용 설정 추가 === citest6 123 8/18 test eee testtest
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
