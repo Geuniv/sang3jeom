ce02abe6564d3c33767fbdc51a73a920b839671b
joohyeongLee <joo9710@naver.com>
2025-08-18 10:00:05 +0900

    test : community ci/cd test

--- backend/community-service/src/main/java/com/sang3jeom/community_service/CommunityServiceApplication.java
+++ backend/community-service/src/main/java/com/sang3jeom/community_service/CommunityServiceApplication.java
@@ -15,7 +15,7 @@ public class CommunityServiceApplication {
 		SpringApplication.run(CommunityServiceApplication.class, args);
 	}
 
-	// === CORS 전역 허용 설정 추가 === citest6
+	// === CORS 전역 허용 설정 추가 === citest6 123
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
